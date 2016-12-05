4c2862101402fefd39d8ca274882fc00a580b078
Sabino Velasquez <sabinovelasquez@gmail.com>
2016-12-04 23:00:02 -0300

    correcciones

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('serve:before', ['sass', 'watch']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
